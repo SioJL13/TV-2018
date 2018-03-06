084dd8ab470e79397a175490d7f79d18dadb74ba
Humberto Atondo <a00959979@itesm.mx>
2018-03-06 16:26:17 -0600

    adding first map draft assets

---
+++ Assets-Mapa/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Input.GetAxis("Vertical") * Vector3.forward * Time.deltaTime * speed, Space.Self);
+        transform.Rotate(0, Input.GetAxis("Horizontal") * 50 * Time.deltaTime, 0, Space.Self);
+	}
+}
