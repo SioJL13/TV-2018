d5b6d85765b146123ed32d7efb46eccb1acd57b1
solvedrex223 <31245247+solvedrex223@users.noreply.github.com>
2018-03-06 16:09:18 -0600

    Add files via upload

---
+++ Boton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boton : MonoBehaviour {
+
+ // This method triggers the switch that turns off the lights
+ // It is implemented with a Pointer Down event within the buttons
+    public void Onoff()
+    {
+        Luz.Apagador();
+
+    }
+
+   
+}
---
+++ Luz.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static  class Luz {
+    //We search for all the Light objects in the scene to control them
+    // Also we have a counter to control which light turns off
+    public static Light [] Luces = (Light []) GameObject.FindObjectsOfType(typeof(Light));
+    public static int contador = 0;
+
+    public static void Apagador ()
+    {
+        Luces[contador].enabled = !Luces[contador].enabled;
+        contador++;
+    }
+
+
+}
