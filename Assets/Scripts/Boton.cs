e7f6b4f0bf83b0746a324926e5042a62bfa70482
Miguel Montoya <miguel.mzbi@gmail.com>
2018-03-06 20:55:37 -0600

    Setup luces

--- Assets/Scripts/Boton.cs
+++ Assets/Scripts/Boton.cs
@@ -1,14 +1,33 @@
 ﻿using UnityEngine;
 
 public class Boton : MonoBehaviour {
+    public Light[] Luces;
 
- // This method triggers the switch that turns off the lights
- // It is implemented with a Pointer Down event within the buttons
+    // This method triggers the switch that turns off the lights
+    // It is implemented with a Pointer Down event within the buttons
     public void Onoff()
     {
-        Luz.Apagador();
+        if (!Luces[Luces.Length - 1].enabled) {
+            for (int i = 0; i < Luces.Length; i++) {
+                if (Luces[i].enabled) {
+                    this.Luces[i].enabled = !this.Luces[i].enabled;
+                    this.Luces[i + 1].enabled = !this.Luces[i + 1].enabled;
+                    i = Luces.Length+1;
+                }
+            }
+        }
+        
 
     }
 
-   
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("Click");
+            Onoff();
+
+        }
+         
+            }
+
 }
--- Assets/Scripts/Luz.cs
+++ Assets/Scripts/Luz.cs
@@ -1,16 +1,20 @@
 ﻿using UnityEngine;
 
-public static  class Luz {
+public class Luz : MonoBehaviour {
     //We search for all the Light objects in the scene to control them
     // Also we have a counter to control which light turns off
-    public static Light [] Luces = (Light []) GameObject.FindObjectsOfType(typeof(Light));
-    public static int contador = 0;
-
-    public static void Apagador ()
+    public  Light[] Luces;
+    //= (Light []) GameObject.FindObjectsOfType(typeof(Light));
+    
+    
+    /*public  void Apagador ()
     {
         Luces[contador].enabled = !Luces[contador].enabled;
-        contador++;
-    }
+        Luces[contador + 1].enabled = !Luces[contador + 1].enabled;
+        Debug.Log(Luces[contador].name);
+        contador--;
+        
+    }*/
 
 
 }
