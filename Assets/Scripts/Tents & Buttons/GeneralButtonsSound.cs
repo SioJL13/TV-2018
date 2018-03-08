ab1a9d3e72b784006173a6b70e40276e4fa6df0f
Miguel Montoya <miguel.mzbi@gmail.com>
2018-03-08 00:41:17 -0600

    Proper sound when near

---
+++ Assets/Scripts/Tents & Buttons/GeneralButtonsSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralButtonsSound : MonoBehaviour {
+
+    public AudioSource tentSound;
+    public float volume;
+
+    // Use this for initialization
+    void Start () {
+        this.tentSound = this.GetComponent<AudioSource>();
+        this.tentSound.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void stopAudio() {
+        this.tentSound.Stop();
+    }
+}
