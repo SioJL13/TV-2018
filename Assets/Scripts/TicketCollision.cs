698d86fe0e5fa0fe32263ebfc912af4c31d37744
Miguel Angel <miguel.mzbi@gmail.com>
2018-03-08 13:26:58 -0600

    Project WITH resources

---
+++ Assets/Scripts/TicketCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TicketCollision : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject spawner;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision col) {
+		print(col.gameObject.name);
+
+    if (col.gameObject.name == "Buzon")
+        {
+
+			player.transform.position = spawner.transform.position;//(where you want to teleport
+        //SceneManager.LoadScene("InGame");
+        }
+	}
+}
